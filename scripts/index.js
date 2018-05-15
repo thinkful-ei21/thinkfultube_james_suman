e5565a6d0529c7bc28415a4ffc017ec934707ae1
Suman Bandita <sumanbandita@gmail.com>
2018-05-15 12:22:53 -0400

    fetch videos

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const API_KEY = 'YOUR_KEY_HERE';
+'use strict';
+const API_KEY = 'AIzaSyCqu7JqABYdWI2X1yhIAEwvCM5MNGtFw0g';
 
 /*
   We want our store to hold a `videos` array of "decorated" objects - i.e. objects that
@@ -18,7 +19,7 @@ const store = {
 
 // TASK: Add the Youtube Search API Base URL here:
 // Documentation is here: https://developers.google.com/youtube/v3/docs/search/list#usage
-const BASE_URL = '';
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 // TASK:
 // 1. Create a `fetchVideos` function that receives a `searchTerm` and `callback`
@@ -26,9 +27,16 @@ const BASE_URL = '';
 // 3. Make a getJSON call using the query object and sending the provided callback in as the last argument
 // TEST IT! Execute this function and console log the results inside the callback.
 const fetchVideos = function(searchTerm, callback) {
-
+  const query = {
+    part : 'snippet',
+    key :API_KEY,
+    maxResults : 25,
+    q: `${searchTerm}`, 
+  };
+  return $.getJSON(BASE_URL, query, callback);  
 };
 
+//fetchVideos('james',() => console.log('testing again'));
 // TASK:
 // 1. Create a `decorateResponse` function that receives the Youtube API response
 // 2. Map through the response object's `items` array
