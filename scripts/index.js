14fb2234e828bd501633b2b0eb84b9ef76991663
Suman Bandita <sumanbandita@gmail.com>
2018-05-16 10:10:36 -0400

    removing render from index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,8 @@
 // When DOM is ready:
 $(document).ready(function () {
   // TASK:
-  // 1. Run `handleFormSubmit` to bind the event listener to the DOM   
-  $(document).ready(function(){
-    VideoList.bindEventListeners();
-    VideoList.render();
-  });
+  // 1. Run `handleFormSubmit` to bind the event listener to the DOM    
+  VideoList.bindEventListeners();  
 });
 
 
